e03f4d1de4e39e5146fdd0e86e31e62de8eb73f6
blaircalderwood <blaircalderwood@live.co.uk>
2016-02-17 16:39:38 +0000

    Removes SK

--- DISS/settings.py
+++ DISS/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gkmzi%4!)7bu*n22n(s)m3(3zm@49l%16p+g)9(-&!g1^xmu0c'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
