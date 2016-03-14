6e7f84114356c2f68ef692eb24493d76af0841b1
asranet <info@asranet.co.uk>
2016-03-14 16:38:14 +0000

    Added author instructions page

--- uploadAbstract/models.py
+++ uploadAbstract/models.py
@@ -2,27 +2,25 @@ from __future__ import unicode_literals
 from django.db import models
 from DISS.email_functionality import email_admin, email_client
 
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
-
 
 class SubmittedAbstract(models.Model):
     salutation = models.CharField(max_length=6)
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
+    primary_author_first_name = models.CharField(max_length=35)
+    primary_author_surname = models.CharField(max_length=35)
+    co_authors_names = models.CharField(max_length=770)
     email = models.EmailField(max_length=60, unique=True)
     paper_title = models.CharField(max_length=60)
     abstract = models.CharField(max_length=2000)
 
     def save(self, *args, **kwargs):
 
-        sorted_self = [["Salutation", self.salutation], ["First name", self.first_name], ["Last Name", self.last_name],
-                       ["Email", self.email], ["Paper Title", self.paper_title], ["Abstract", self.abstract],
+        sorted_self = [["Salutation", self.salutation], ["Primary Author First Name", self.primary_author_first_name],
+                       ["Primary Author Surname", self.primary_author_surname],
+                       ["Co Authors Names", self.co_authors_names], ["Email", self.email],
+                       ["Paper Title", self.paper_title], ["Abstract", self.abstract],
                        ]
 
-        email_client(self, "DISS 2017 Abstract Upload", "You have uploaded an abstract.")
+        email_client(self, "DISS 2017 Abstract Upload", "Thank you for uploading your abstract for DISS 2017")
         email_admin(self, "New DISS 2017 Abstract", "Please find enclosed the details for the new DISS "
                                                     "2017 abstract upload.", sorted_self)
 
